180bfb9d00656885927020240a7889da169bdb5d
Kadin Wilson <kadin.t.wilson@gmail.com>
2025-12-05 17:37:28 -0800

    Day 5 Part 2

--- go/day5/main.go
+++ go/day5/main.go
@@ -23,20 +23,16 @@ func main() {
 		return
 	}
 
-	fresh := 0
-	for _, id := range ids {
-		if r.inRange(id) {
-			fresh++
-		}
-	}
-
-	fmt.Println(fresh)
+	fmt.Printf(
+		"available: %d\npossible: %d\n",
+		r.availableCount(ids),
+		r.possibleCount(),
+	)
 }
 
 type ranges struct {
-	count int
-	low   []int
-	high  []int
+	low  []int
+	high []int
 }
 
 func (r *ranges) inRange(n int) bool {
@@ -49,48 +45,107 @@ func (r *ranges) inRange(n int) bool {
 	return false
 }
 
-func parseRange(s string) (int, int, error) {
+func (r *ranges) availableCount(ids []int) int {
+	count := 0
+	for _, id := range ids {
+		if r.inRange(id) {
+			count++
+		}
+	}
+	return count
+}
+
+func (r *ranges) possibleCount() int {
+	count := 0
+	for i, l := range r.low {
+		h := r.high[i]
+		count += h - l + 1
+	}
+	return count
+}
+
+type pair struct {
+	low  int
+	high int
+}
+
+func (p pair) overlap(pp pair) bool {
+	if p.low >= pp.low && p.low <= pp.high {
+		return true
+	}
+	if p.high >= pp.low && p.high <= pp.high {
+		return true
+	}
+	if p.low <= pp.low && p.high >= pp.high {
+		return true
+	}
+	return false
+}
+
+func (p pair) join(pp pair) (pair, error) {
+	if !p.overlap(pp) {
+		return pair{}, fmt.Errorf("pairs %v and %v do not overlap", p, pp)
+	}
+	return pair{low: min(p.low, pp.low), high: max(p.high, pp.high)}, nil
+}
+
+func parsePair(s string) (pair, error) {
 	nums := strings.Split(s, "-")
 	if len(nums) != 2 {
-		return 0, 0, fmt.Errorf("range %s is improperly formatted", s)
+		return pair{}, fmt.Errorf("range %s is improperly formatted", s)
 	}
 
 	l, err := strconv.Atoi(nums[0])
 	if err != nil {
-		return 0, 0, fmt.Errorf("%s in %s is not a number", nums[0], s)
+		return pair{}, fmt.Errorf("%s in %s is not a number", nums[0], s)
 	}
-	r, err := strconv.Atoi(nums[1])
+	h, err := strconv.Atoi(nums[1])
 	if err != nil {
-		return 0, 0, fmt.Errorf("%s in %s is not a number", nums[1], s)
+		return pair{}, fmt.Errorf("%s in %s is not a number", nums[1], s)
 	}
 
-	if l > r {
-		return 0, 0, fmt.Errorf("low val %d is greater than high val %d", l, r)
+	if l > h {
+		return pair{}, fmt.Errorf("low val %d is greater than high val %d", l, h)
 	}
 
-	return l, r, nil
+	return pair{low: l, high: h}, nil
 }
 
 func scanRanges(scn *bufio.Scanner) (*ranges, error) {
-	r := ranges{
-		low:  []int{},
-		high: []int{},
-	}
+	count := 0
+
+	rm := map[pair]struct{}{}
 
 	for scn.Scan() {
 		if len(scn.Text()) == 0 {
 			break
 		}
 
-		r.count++
+		count++
 
-		l, h, err := parseRange(scn.Text())
+		p, err := parsePair(scn.Text())
 		if err != nil {
-			return nil, fmt.Errorf("failed to parse range #%d: %w", r.count, err)
+			return nil, fmt.Errorf("failed to parse range #%d: %w", count, err)
 		}
 
-		r.low = append(r.low, l)
-		r.high = append(r.high, h)
+		for pp := range rm {
+			if p.overlap(pp) {
+				p, _ = p.join(pp)
+				delete(rm, pp)
+			}
+		}
+
+		rm[p] = struct{}{}
+	}
+
+	r := ranges{
+		low:  []int{},
+		high: []int{},
+	}
+
+	for p := range rm {
+		r.low = append(r.low, p.low)
+		r.high = append(r.high, p.high)
 	}
 
 	return &r, nil
